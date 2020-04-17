35e8cf434c3918dad35be98bd4396916d098cb4c
Jozy Kinnaman <jozypants@gmail.com>
2020-04-17 10:47:48 -0700

    returns a user's age in Mercury years test setup. Expect to fail.

--- __tests__/superGalCal.test.js
+++ __tests__/superGalCal.test.js
@@ -6,4 +6,10 @@ describe('ageCalulator', () => {
     let newAge = new ageCalulator (34);
     expect(newAge.age).toEqual(34);
   });
+
+  test("should correctly return a user's age in Mercury years", ()=> {
+    let newAge = new ageCalulator(34);
+    var mercAge = newAge.mercCalc();
+    expect(mercAge).toEqual(141);
+  })
 });
